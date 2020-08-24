c52d0e6a3a59a39da145636aac19b8d2f16b0592
Kseniya <kseniya9696@gmail.com>
2020-08-24 08:37:01 +0100

    Fix axios error

--- deals-front/src/App.js
+++ deals-front/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   const getDBConection = async () => {
     axios.get(environment.url + "/getData").then(res => {
-      setDbConnectionMsgOpen(res.isSuccsesfull);
+      setDbConnectionMsgOpen(res.isSuccessfull);
     });
   };
 
