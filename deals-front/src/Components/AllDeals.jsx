c0385eccc086cbfe0ea5415ea5863bd71b052952
Kseniya <kseniya9696@gmail.com>
2020-08-24 13:50:42 +0100

    Add refresh btn to all deals component

--- deals-front/src/Components/AllDeals.jsx
+++ deals-front/src/Components/AllDeals.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Deal from './Deal';
 import {environment} from '../utils';
 import axios from 'axios';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from'@material-ui/core';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button} from'@material-ui/core';
 
 const AllDeals = props =>{
     const [deals, setDeals] = useState([]);
@@ -14,10 +14,8 @@ const AllDeals = props =>{
     const getDeals = async () => {
         try {
           const res = await axios.get(environment.url + "/getTopDeals");
-          const deals = [];
-          let newDeal = await res.data;
-          deals.push(newDeal);
-          setDeals(deals);
+          let dealsArr = await res.data;
+          setDeals(dealsArr);
         } catch (e) {
         }
     };
@@ -28,21 +26,24 @@ const AllDeals = props =>{
         />
     ));
     return(
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Instrument Name</TableCell>
-                        <TableCell>Contragent</TableCell>
-                        <TableCell>Price</TableCell>
-                        <TableCell>Type</TableCell>
-                        <TableCell>Quantity</TableCell>
-                        <TableCell>Time</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>{dealsElements}</TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <Button onClick={getDeals}> Refresh</Button>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Instrument Name</TableCell>
+                            <TableCell>Contragent</TableCell>
+                            <TableCell>Price</TableCell>
+                            <TableCell>Type</TableCell>
+                            <TableCell>Quantity</TableCell>
+                            <TableCell>Time</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>{dealsElements}</TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
 
